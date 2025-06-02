ae691aadec4904d0b03c9d7e6a8bbe790afe46ef
ZnEb9 <155696667+ZnEb9@users.noreply.github.com>
2025-06-02 22:33:30 +0700

    Update header.php

--- api/header.php
+++ api/header.php
@@ -7,12 +7,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="style.css" rel="stylesheet">
+    <link href="/public/style.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="index.php">Praktikum Web</a>
+            <a class="navbar-brand" href="/">Praktikum Web</a>
         </div>
     </nav>
     <div class="container">
