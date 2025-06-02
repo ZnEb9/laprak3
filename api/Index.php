810a823dbdb780466065c2cc5ef08d16872fbafd
ZnEb9 <155696667+ZnEb9@users.noreply.github.com>
2025-06-02 22:21:03 +0700

    Update Index.php

--- api/Index.php
+++ api/Index.php
@@ -2,7 +2,7 @@
 
 <div class="container mt-5">
     <h2 class="text-center mb-4">Form Login</h2>
-    <form action="welcome.php" method="post" class="col-md-6 mx-auto">
+    <form action="api/welcome.php" method="post" class="col-md-6 mx-auto">
         <div class="mb-3">
             <label for="nama" class="form-label">Nama Lengkap</label>
             <input type="text" class="form-control" id="nama" name="nama" >
@@ -15,4 +15,4 @@
     </form>
 </div>
 
-<?php include 'footer.php'; ?>
+<?php include 'api/footer.php'; ?>
